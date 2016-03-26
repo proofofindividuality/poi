6b7b2e2f3d899d0957be2de561486b4b4949250b
proofofindividuality <proofofindividuality@yopmail.com>
2016-03-26 05:42:57 +0100

    Rename poi.sol to subconracts/poi.sol

---
+++ subconracts/poi.sol
@@ -0,0 +1,75 @@
+contract poi {
+    
+    address currentPOItokens;
+    address currentRound;
+    address depositGovernanceContract;
+    address previousDepositGovernance;	    
+    
+    
+    uint genesisblock;
+    uint roundLength;
+    uint registrationPeriod;
+    uint hangoutCountdown;
+    uint nextRound;
+    
+    uint depositSize;
+    uint groupSize;
+    
+    function poi (){
+        genesisblock = block.number;
+        roundLength = 172800; // set POI pseudonym parties to happen once a month
+        nextRound = genesisblock;
+        
+        depositSize = 10;
+        registrationPeriod = roundLength * 29/30; // 29 days
+		hangoutCountdown = registrationPeriod * 23/24; // 23 hours
+		groupSize = 5;
+	
+        scheduleRound();
+    }
+    
+    
+    function scheduleRound() {
+        if(block.number < nextRound) throw;
+        if(currentRound != 0) registration(currentRound).endRound();
+        currentRound = new registration(depositSize, registrationPeriod, hangoutCountdown, groupSize);
+        
+        nextRound += roundLength;
+    }
+
+    function issuePOIs(address[] verifiedUsers) {
+        if(msg.sender != currentRound) throw;
+        if(currentPOItokens != 0) generatePOIs(currentPOItokens).depricatePOIs;
+        currentPOItokens = new generatePOIs(verifiedUsers);
+        
+        // now that the a new POI round has begun and the deposits have been returned,
+        // launch a new depositGovernanceContract
+        // if a new depositSize has been agreed on, the old depositGovernanceContract will automatically
+        // invoke the newDepositSize() function (see below) 
+        
+        newDepositGovernanceContract();
+    }
+    
+    function newDepositGovernanceContract() internal{
+        if(depositGovernanceContract != 0) {
+        	depositGovernance(depositGovernanceContract).processProposals();
+        	previousDepositGovernance = depositGovernanceContract; // processProposals() will take a few minutes, so use a temporary address, previousDepositGovernance, for newDepositSize() for now
+        }
+        depositGovernanceContract = new depositGovernance();
+        /* deposits paid into voting for depositSizes should be deducatable from the deposit required to register */
+        /* that's not implemented yet. stub on https://gist.github.com/resilience-me/0afcb1d692bb815de9ed */
+    }
+
+    function newDepositSize(uint newDepositSize){
+    if(msg.sender != previousDepositGovernance) throw;
+        depositSize = newDepositSize;
+    }
+    
+    function verifyPOI (address v) returns (string){
+	    if (generatePOIs(currentPOItokens).balanceOf(v)==0){
+		    return "account does not have a valid POI";
+	    }
+    	else return "account has a valid POI";
+    }      
+    
+}
