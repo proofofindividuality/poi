b59f959c83b12c57e1a62786c17551a5bf1ce5a4
resilience-me <emailsareobsolete@gmail.com>
2016-05-21 15:21:13 +0200

    Update poi.sol

--- poi.sol
+++ poi.sol
@@ -386,39 +386,38 @@ contract generatePOIs {
    
 }
 
-/* manage all deposits in one contract. allow users to vote using the ether from their registration deposit, if they want. 
-   return all deposits after the round has finished. */
 
+/*  
+    All deposits are managed by one contract. Upon registration, the anti-spam deposit is automatically used to vote for the current
+    depositSize. This auto-vote can then be changed and used to vote for another proposal, or to create a new proposal. At the end of
+    each round, proccessProposals() is called and the proposal that has the highest number of up-votes, measured in ether, becomes 
+    the new depositSize. proposals[0] will probably win most of the time, unless there is an active community effort to change the 
+    depositSize 
+*/
+   
 contract depositGovernance {
 
 address registrationContract;
 address poiContract;
 
-struct Participant {
-      address participant;
-      uint256 antiSpamDeposit;
-      uint256 Votes;  // deposited ether that has been used to vote
-}
 
-Participant[] public participants;
+address[] participants;
+mapping(address => bool) participantIndex;
 
-mapping (address => uint256) public participantID;
-
+mapping(address => uint256) public AutoVote;
+mapping(address => uint256) public Votes;
 
 struct proposeNewDeposit {
     uint256 depositSize;
     uint256 votesInFavour; // in ether
     uint256 votesAgainst;  // in ether
-
 }
 
 proposeNewDeposit[] public proposals;
 
-function depositGovernance(uint currentDepositSize){
+function depositGovernance(uint currentDepositSize, address registrationContract){
     poiContract = msg.sender;
-    participants.length++;
-    participants[0] = Participant({participant: 0, antiSpamDeposit: 0, Votes: 0});
-    
+    registrationContract = registrationContract;
     proposals.push(proposeNewDeposit({
              depositSize: currentDepositSize,
              votesInFavour: 0,
@@ -426,63 +425,50 @@ function depositGovernance(uint currentDepositSize){
          }));
 }
 
-// contract registration calls registrationDeposit(msg.sender).value(msg.value)
 
-function registrationDeposit(address registrant)  {
+function registrationDeposit(address registrant) external {
 if(msg.sender != registrationContract) throw;   
-if(participantID[registrant] == 0) {
-      participantID[registrant] = participants.length;
-      participants.push(Participant({
-               participant: registrant,
-               antiSpamDeposit: msg.value / 1 ether,
-               Votes: 0
-           }));
-   }
-   else {
-      participants[participantID[registrant]].antiSpamDeposit += msg.value / 1 ether;
-   }
-   proposals[0].votesInFavour += msg.value / 1 ether;
-   
+if(participantIndex[registrant] == false) participants.push(msg.sender); participantIndex[msg.sender] = true;
+
+/* the registrant automatically uses their anti-spam deposits to vote for the current depositSize */
+/* they can move their auto-vote to vote for other proposals if they wish, by using voteOnProposal() */
+    AutoVote[registrant] += msg.value;
+    proposals[0].votesInFavour += msg.value;
 }
 
-function NewProposal(uint256 depositSize){
-        uint availableEther = participants[participantID[msg.sender]].antiSpamDeposit + msg.value / 1 ether;
+function NewProposal(uint256 depositSize) public {
+        uint availableEther = AutoVote[msg.sender] + msg.value;
         if(availableEther < depositSize) throw;
+        if(participantIndex[msg.sender] == false) participants.push(msg.sender); participantIndex[msg.sender] = true;
         proposals.push(proposeNewDeposit({
             depositSize: depositSize,
             votesInFavour: depositSize,
             votesAgainst: 0
         }));
-        participants[participantID[msg.sender]].antiSpamDeposit -= availableEther - msg.value / 1 ether;
-        proposals[0].votesInFavour -= availableEther - msg.value / 1 ether;
-        participants[participantID[msg.sender]].Votes += depositSize;
+        AutoVote[msg.sender] -= (availableEther - msg.value);
+        proposals[0].votesInFavour -= (availableEther - msg.value);
+        Votes[msg.sender] += depositSize;
 }
 
 
 
-function voteOnProposal(uint proposalIndex, bool opinion, uint amount) {
-        if(participantID[msg.sender] == 0) {
-              participantID[msg.sender] = participants.length;
-              participants.push(Participant({
-                       participant: msg.sender,
-                       antiSpamDeposit: 0,
-                       Votes: 0
-                   }));
-           
-        }   
-        uint availableEther = participants[participantID[msg.sender]].antiSpamDeposit + msg.value / 1 ether;
+function voteOnProposal(uint proposalIndex, bool opinion, uint amount) public {
+   
+        uint availableEther = AutoVote[msg.sender] + msg.value;
         if(availableEther < amount) amount = availableEther; 
-        
+        if(participantIndex[msg.sender] == false) participants.push(msg.sender); participantIndex[msg.sender] = true;
+
         if(opinion == true)
         proposals[proposalIndex].votesInFavour += amount;
         else
         proposals[proposalIndex].votesAgainst += amount;
-        participants[participantID[msg.sender]].antiSpamDeposit -= amount - (msg.value / 1 ether);
-        proposals[0].votesInFavour -=  amount - (msg.value / 1 ether);
-        participants[participantID[msg.sender]].Votes += amount;
+        
+        AutoVote[msg.sender] -= (amount - msg.value);
+        proposals[0].votesInFavour -=  (amount - msg.value);
+        Votes[msg.sender] += amount;
 }
 
-function processProposals() { // invoked at the end of each round
+function processProposals() external { // invoked at the end of each round
     if(msg.sender != poiContract) throw;
     uint iterateToHighest;
     
@@ -501,11 +487,12 @@ function processProposals() { // invoked at the end of each round
     /* then return deposits */
     
         for (uint k = 1; k < participants.length; k++){
-            uint totalDeposit = participants[k].antiSpamDeposit + participants[k].Votes;
-            participants[k].participant.send(totalDeposit * 1 ether);
+            uint totalDeposit = AutoVote[participants[k]] + Votes[participants[k]];
+            participants[k].send(totalDeposit);
          }
+         
     /* then suicide contract */
-    if(this.balance == 0) suicide(poiContract);
+    suicide(poiContract);
 }
 
 
